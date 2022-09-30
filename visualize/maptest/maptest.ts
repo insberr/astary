6d2c3dbd6c088e9d54891924d19904c9c456bf0f
Wackery <zapcontact000@gmail.com>
2022-09-30 16:27:01 +0000

    make it a part

--- visualize/maptest/maptest.ts
+++ visualize/maptest/maptest.ts
@@ -3,7 +3,7 @@ import { Raycast, svgToPaths, generateNodes, generateWalls, Node, AStar } from "
 import _d from "data-url:./map.svg"; // might be a good idea to do a url and not bundle it in
 // @ts-ignore
 import _dt from "bundle-text:./map.svg";
-
+// having two imports means that the map svg is included twice in the bundle, maybe convert from text to data url?
 const d: string = _d;
 // can you import the file or like uh .. how to get svg as string ???
 //import eruda from "../eruda";
